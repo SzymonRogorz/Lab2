4d581166f33f37def726640563b0bd504ec7fed0
SzymonRogorz <gdx162469@student.gdansk.merito.pl>
2024-10-19 13:05:38 +0200

    Test commit

---
+++ script.js
@@ -0,0 +1,13 @@
+function exampleFunction(param1,param2){
+    if(param1===param2){
+        console.log("Parameters are equal");
+    }else{
+        console.log("Parameters are not equal");
+    }
+}
+
+const arr=[1,2,3,4,5];
+
+arr.forEach(function(item){
+    console.log(item);
+});
